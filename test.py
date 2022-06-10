32762fd34cfee354a427c863a63a12a3475328d9
Dougisnotavailable <96440626+Dougisnotavailable@users.noreply.github.com>
2022-06-10 15:08:16 -0300

    Add files via upload

---
+++ test.py
@@ -0,0 +1,27 @@
+import pandas as pd 
+import matplotlib.pyplot as plt 
+import numpy as np 
+import statsmodels.api as sm
+import math 
+
+from sklearn.model_selection import train_test_split
+from sklearn.linear_model import LinearRegression
+from sklearn.metrics import mean_squared_error, r2_score, mean_absolute_error
+
+from scipy import stats
+from scipy.stats import kurtosis, skew
+
+# loading the data
+data = pd.read_csv (r'C:\Users\User\Desktop\Oil\CrudeOilExport_1908-2020.csv')
+print (data)
+
+#EXPLORE
+#How is the data distributed?
+#Does there appear to be a relationship between the two variables?
+#Are there any outliers?
+#Is the data skewed?
+
+# define the x & y data.
+#x = data['month']
+#y = data['rate']
+
